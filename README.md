c1d6b1f5806d14fbf0da9d21cfe068bf58e51728
lukeexer <lientuna@gmail.com>
2022-05-04 22:46:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# py_mock_example
---
+++ patch_example.py
@@ -0,0 +1,18 @@
+from unittest.mock import patch
+
+class Class:
+    def method(self):
+        pass
+
+with patch('__main__.Class') as MockClass:
+    instance = MockClass()
+    instance.method.return_value = 'foo'
+
+    print(Class() is instance)
+    print(instance.method())
+    print(instance.method())
+    print(instance.method.call_count)
+
+    print(instance.method('a')) # Modify the argument 'a' as 'b' for testing the assertion.
+    instance.method.assert_called_with('a'), '''the instance.method hasn't been called with 'a'.'''
+
