bf5e01c296d361590ad0a9a078e7494876f19797
Luke <49667399+lukeexer@users.noreply.github.com>
2022-05-09 16:37:40 +0800

    add method mock example.

--- patch_example.py
+++ patch_example.py
@@ -16,3 +16,6 @@ with patch('__main__.Class') as MockClass:
     print(instance.method('a')) # Modify the argument 'a' as 'b' for testing the assertion.
     instance.method.assert_called_with('a'), '''the instance.method hasn't been called with 'a'.'''
 
+with patch('__main__.Class.method') as mock_method:
+
+    mock_method.return_value = 'foo'
